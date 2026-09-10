7cc017e116ecbb3b40b91d088c8e6563ff93be43
TETASS237 <tekeutassouyvan@gmail>
2026-09-10 11:58:36 +0100

    Devoir javascripts

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+const cont = "Hello from inline";
+console.log("Hello from inline JS");
+
+let age = 25;
+let name = "Yvan";
+let student = "developer student";
+console.log(`age = ${age} | Type = ${typeof age}`);
+console.log(`name = ${name} | Type = ${typeof name}`);
+console.log(`student = ${student} | Type = ${typeof student}`);
+
+let number1 = 5;
+let number2 = 7;
+let result1 = number1 + number2;
+let result2 = number1 * number2;
+let result3 = number2 - number1;
+let result4 = number1 % number2;
+console.log(`result1 = ${result1}`);
+console.log(`result2 = ${result2}`);
+console.log(`result3 = ${result3}`);
+console.log(`result4 = ${result4}`);
+
+
+console.log(5 == "5"); // egalite simple
+console. log(5 === "5"); // egalite stricte
+
+
+console.table({age: age, name: name, student: student});
+
+
+
